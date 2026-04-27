428469116cf08e7928c94d3897ec1773f5ef28e3
Salwa khan <khansalwa2005@gmail.com>
2026-04-27 13:27:28 +0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -35,19 +35,24 @@ exports.index = function (req, res, next) {
 };
 
 exports.loginHandler = function (req, res, next) {
-  if (validator.isEmail(req.body.username)) {
-    User.find({ username: req.body.username, password: req.body.password }, function (err, users) {
-      if (users.length > 0) {
-        const redirectPage = req.body.redirectPage
-        const session = req.session
-        const username = req.body.username
-        return adminLoginSuccess(redirectPage, session, username, res)
+  // 1. Force the inputs to be strings. 
+  // If req.body.username is an object like {$gt: ""}, String() turns it into "[object Object]"
+  const username = String(req.body.username);
+  const password = String(req.body.password);
+
+  if (validator.isEmail(username)) {
+    // 2. Use the forced string variables in the query instead of req.body directly
+    User.find({ username: username, password: password }, function (err, users) {
+      if (users && users.length > 0) {
+        const redirectPage = req.body.redirectPage;
+        const session = req.session;
+        return adminLoginSuccess(redirectPage, session, username, res);
       } else {
-        return res.status(401).send()
+        return res.status(401).send();
       }
     });
   } else {
-    return res.status(401).send()
+    return res.status(401).send();
   }
 };
 
